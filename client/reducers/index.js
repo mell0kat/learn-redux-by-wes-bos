5e30c143652f1ab598d779dd445553a9068c77bb
Katherine Mello <katherine.mello8@gmail.com>
2016-09-02 11:13:17 -0400

    reducers added

---
+++ client/reducers/index.js
@@ -0,0 +1,21 @@
+// action is dispatched
+// --> reducers update state (store)
+// reducers = listeners
+// one reducer per "pieve of state" (piece of data from backend / model)
+
+// make root reducer
+
+import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+
+import posts from './posts';
+import comments from './comments';
+
+const rootReducer = combineReducers({
+  posts,
+  comments,
+  router: routerReducer
+});
+
+export default rootReducer;
+
---
+++ client/reducers/posts.js
@@ -0,0 +1,8 @@
+// reducers take the urrent state and the action
+
+function posts(state = [], action) {
+	console.log(state, action);
+	return state;
+}
+
+export default posts;
